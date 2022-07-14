567fe2495057c53d2aa55aab21797f38ee14d52e
shobman17 <210070081@iitb.ac.in>
2022-07-15 01:40:42 +0530

    Add BT files

--- communication/interface_transmit/interface_transmit.ino
+++ communication/interface_transmit/interface_transmit.ino
@@ -1,8 +1,10 @@
 #include <BluetoothSerial.h>
-#include "mpu9250.h"
+#include <MPU9250_WE.h>
+#include <Wire.h>
+#define MPU9250_ADDR 0x68
 
 BluetoothSerial SerialBT; //Initialise BT object
-bfs::Mpu9250 imu; //Initialise mpu object
+MPU9250_WE sensor = MPU9250_WE(MPU9250_ADDR); //Initialise sensor object 
 long int refTime = millis(); //Start time
 
 void setup() {
@@ -14,7 +16,7 @@ void setup() {
   SerialBT.begin("Shobhit_ESP32");
   Serial.println("Device is ready to connect");
   Serial.println("Waiting for response");
-  while(!SerialBT.available()){ // Wait for response from receiver
+    while(!SerialBT.available()){ // Wait for response from receiver
     Serial.print(".");
     delay(1000);
   }
@@ -23,56 +25,73 @@ void setup() {
   //Start i2c bus
   Wire.begin(); // Default values are SCL: P22, SDA: P21
   Wire.setClock(400000);
-  imu.Config(&Wire, bfs::Mpu9250::I2C_ADDR_PRIM); // Default address is 0x68
-  
-  /* Initialize and configure IMU */
-  if (!imu.Begin()) {
-    Serial.println("Error initializing communication with IMU");
-    while(1) {}
+  if (!sensor.init()) {
+      Serial.println("Error connected to MPU9250");
+  } else {
+      Serial.println("MPU9250 connected succesfully");   
   }
-  
-  /* Set the sample rate divider */
-  if (!imu.ConfigSrd(19)) {
-    Serial.println("Error configured SRD");
-    while(1) {}
+  if(!sensor.initMagnetometer()){
+    Serial.println("Magnetometer does not respond");
   }
+  else{
+    Serial.println("Magnetometer is connected");
+  }
+
+  // Start auto calibration
+  Serial.println("Position you MPU9250 flat and don't move it - calibrating...");
+  delay(1000);
+  sensor.autoOffsets();
+  Serial.println("Done!");
+
+  sensor.setSampleRateDivider(5); // Sample rate = internal rate/(divider + 1)
+  sensor.setAccRange(MPU9250_ACC_RANGE_4G); //Can be changed to 2G, 4G, 8G, 16G
+  sensor.enableAccDLPF(true); // Enable the digital low pass filter. Wow
+  sensor.setAccDLPF(MPU9250_DLPF_6);
+  sensor.enableGyrDLPF(); // Wow
+  sensor.setGyrDLPF(MPU9250_DLPF_6); // Lowest noise level apparently
+  sensor.setGyrRange(MPU9250_GYRO_RANGE_500); // Can be changed to 250, 500, 1000, 2000 deg per sec
+  sensor.setMagOpMode(AK8963_CONT_MODE_100HZ);
+  delay(200);
   
   pinMode(LED_BUILTIN, OUTPUT); // Initialise Inbuilt LED 
+
+  SerialBT.println("Values are as [Time][Gx][Gy][Gz][Ax][Ay][Az][ATotal][Mx][My][Mz]");
 }
 
 void loop() {
-   if (imu.Read()) {
-      // read and transmit sensor data
-      long int currentTime = millis() - refTime;
-      digitalWrite(LED_BUILTIN, HIGH);
-      SerialBT.print(currentTime);
-      SerialBT.print("\t");
-      SerialBT.print(imu.new_imu_data());
-      SerialBT.print("\t");
-      SerialBT.print(imu.new_mag_data());
-      SerialBT.print("\t");
-      SerialBT.print(imu.accel_x_mps2());
-      SerialBT.print("\t");
-      SerialBT.print(imu.accel_y_mps2());
-      SerialBT.print("\t");
-      SerialBT.print(imu.accel_z_mps2());
-      SerialBT.print("\t");
-      SerialBT.print(imu.gyro_x_radps());
-      SerialBT.print("\t");
-      SerialBT.print(imu.gyro_y_radps());
-      SerialBT.print("\t");
-      SerialBT.print(imu.gyro_z_radps());
-      SerialBT.print("\t");
-      SerialBT.print(imu.mag_x_ut());
-      SerialBT.print("\t");
-      SerialBT.print(imu.mag_y_ut());
-      SerialBT.print("\t");
-      SerialBT.print(imu.mag_z_ut());
-      SerialBT.print("\t");
-      SerialBT.print(imu.die_temp_c());
-      SerialBT.print("\n");
-      digitalWrite(LED_BUILTIN, LOW);
-   }
-   
-   delay(50);
+  digitalWrite(LED_BUILTIN, HIGH);
+  xyzFloat aValue = sensor.getGValues();
+  xyzFloat gyr = sensor.getGyrValues();
+  xyzFloat magValue = sensor.getMagValues();
+  float temp = sensor.getTemperature();
+  float resultantG = sensor.getResultantG(aValue);
+  long int currentTime = (millis() - refTime);
+
+  SerialBT.print(currentTime);
+  SerialBT.print(" ");
+  
+  SerialBT.print(gyr.x);
+  SerialBT.print(" ");
+  SerialBT.print(gyr.y);
+  SerialBT.print(" ");
+  SerialBT.print(gyr.z);
+  SerialBT.print(" ");
+  
+  SerialBT.print(aValue.x);
+  SerialBT.print(" ");
+  SerialBT.print(aValue.y);
+  SerialBT.print(" ");
+  SerialBT.print(aValue.z);
+  SerialBT.print(" ");
+  
+  SerialBT.print(magValue.x);
+  SerialBT.print(" ");
+  SerialBT.print(magValue.y);
+  SerialBT.print(" ");
+  SerialBT.print(magValue.z);
+  SerialBT.println(" ");
+  
+  digitalWrite(LED_BUILTIN, LOW);
+
+  delay(500);
 }
