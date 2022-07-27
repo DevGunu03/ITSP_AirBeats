a1ac722de6463e6403b68f92afc52c607f8550f7
shobman17 <210070081@iitb.ac.in>
2022-07-28 00:03:55 +0530

    Add drums

---
+++ MATLAB files/drum_test.m
@@ -0,0 +1,19 @@
+clear;
+
+[y1,fs1]=audioread("Closed-HiHat.wav");
+[y2, fs2] = audioread("Hand-clap.wav");
+[y3,fs3] = audioread("Kick-drum-1_1.wav")
+for i = 1:10
+    sound(y3, fs3);
+    pause(0.25)
+    sound(y2(1105:5000, :), fs2);
+    pause(0.25)
+    sound(y3, fs3);
+    pause(0.17)
+    sound(y3, fs3);
+    pause(0.17)
+    sound(y3, fs3);
+    pause(0.17)
+    sound(y2(1105:5000, :), fs2);
+    pause(0.25)
+end
