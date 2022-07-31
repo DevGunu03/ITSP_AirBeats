cd2199e2d9929c177912d2d37563da6d82d2f413
baswaraj Swamy <baswarajswamy@baswarajs-MacBook-Pro.local>
2022-07-31 19:32:15 +0530

    filter accel data 1.0

--- MATLAB files/drum_test.m
+++ MATLAB files/drum_test.m
@@ -1,10 +1,10 @@
 clear;
 
-[y1,fs1]=audioread("Closed-HiHat.wav");
-[y2, fs2] = audioread("Hand-clap.wav");
-[y3,fs3] = audioread("Kick-drum-1_1.wav")
+[y1,fs1] = audioread("Drums/Closed-HiHat.wav");
+[y2,fs2] = audioread("Drums/Hand-clap.wav");
+[y3,fs3] = audioread("Drums/Kick-drum-1_1.wav");
 for i = 1:10
-    sound(y3, fs3);
+    sound(y3(1105:4608, :), fs3);
     pause(0.25)
     sound(y2(1105:5000, :), fs2);
     pause(0.25)
--- MATLAB files/test.m
+++ MATLAB files/test.m
@@ -1,5 +1,10 @@
 load mpuCali.mat sensorCalibration;
 
+plot(sensorCalibration)
+%legend("ax","ay","az")
+
+%% 
+
 pp = poseplot;
 xlabel("North-x (m)");
 ylabel("East-y (m)");
@@ -10,11 +15,16 @@ Fs = 10;
 decim = 1;
 fuse = ahrsfilter("SampleRate", 10, "DecimationFactor", decim);
 
+
 %Create orientation matrix
 pos = [0 0 0];
-for c = 1:size(sensorCalibration, 1)
+for c = 2:size(sensorCalibration, 1)
     reading = sensorCalibration(c, :);
     q = fuse(reading(5:7)*9.81, reading(2:4)*(pi/180), reading(8:10));
+    g = [0 0 9.81];
+    qg = quaternion([0 g]);
+    g_ = q*qg*q.conj;
+    [r, g_x, g_y, g_z] = parts(g_)
     set(pp, Orientation = q, Position = pos);
     drawnow;
 end
